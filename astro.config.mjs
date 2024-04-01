b5f578aae3819f62c7ff17d64f2cc89f9899d6e6
byzocker <xbox3604707@gmail.com>
2024-04-01 16:39:08 +0000

    bit of tinkering > added robots.txt (via astro-robots-txt) > added sitemap > added dayjs (for remaining time of spotify song) > added other activites besides spotify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://byzocker.de',
+  integrations: [robotsTxt({
+    host: 'byzocker.de',
+    policy: [{
+      userAgent: '*',
+      allow: '/',
+      crawlDelay: 2
+    }]
+  }), sitemap()]
+});
